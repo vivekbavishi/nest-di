da0777c78da1d0f7d9c2c1ccdb02214bcb741eb5
vivek bavishi <vivekbavishi1998@gmail.com>
2026-09-25 10:01:18 +0530

    feat: update README and AppService to include deployment link and GitHub URL

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+        const githubUrl = 'https://github.com/vivekbavishi/nest-di';
+    return `<a href="${githubUrl}" target="_blank" rel="noopener noreferrer">${githubUrl}</a>`;
   }
 }
